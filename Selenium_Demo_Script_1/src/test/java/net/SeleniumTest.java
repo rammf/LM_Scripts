78b65b3b96d37c842a6e638f79cfa0151ab74aa6
rammf <rameshhp18us@gmail.com>
2018-11-08 14:31:58 -0500

    Update SeleniumTest.java

--- Selenium_Demo_Script_1/src/test/java/net/SeleniumTest.java
+++ Selenium_Demo_Script_1/src/test/java/net/SeleniumTest.java
@@ -5,15 +5,32 @@ import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 
+import junit.framework.Assert;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Navigation;
+import org.openqa.selenium.WebDriver.Options;
+import org.openqa.selenium.WebDriver.Window;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
 
 import static junit.framework.TestCase.assertTrue;
 
 public class SeleniumTest {
 
+    private static WebDriver driver;
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
 
+        driver = new ChromeDriver();
     }
 
     @AfterClass
@@ -33,12 +50,14 @@ public class SeleniumTest {
     @Test
     public void demoseltest() throws Exception {
 
-	   // System.setProperty("webdriver.chrome.driver", "/home/demo/chromedriver/2.35/chromedriver");
-	    
-	    
-	    System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+	    //System.setProperty("webdriver.chrome.driver", "/home/demo/chromedriver/2.35/chromedriver");
 
-          WebDriver driver = new ChromeDriver();
+       // System.setProperty("webdriver.chrome.driver", "/home/demo/chromedriver");
+	    
+	    
+	   // System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+
+       //   WebDriver driver = new ChromeDriver();
 
         driver.get("http://www.google.com");
 
@@ -46,9 +65,13 @@ public class SeleniumTest {
 		
 		driver.findElement(By.name("q")).sendKeys(Keys.TAB);
 
-        driver.findElement(By.name("btnK")).click();
+        driver.findElement(By.xpath("//FORM[@id=\"tsf\"]/DIV[2]/DIV[1]/DIV[3]/CENTER[1]/INPUT[1]")).click();
 
-        driver.findElement(By.xpath("//*[@id='rso']/div[1]/div/div/div/div/h3/a")).click();
+		//Button googleSearchButton = browser.describe(Button.class, new XPathDescription("//FORM[@id=\"tsf\"]/DIV[2]/DIV[1]/DIV[3]/CENTER[1]/INPUT[1]"));
+
+     //   driver.findElement(By.name("btnK")).click();
+
+        driver.findElement(By.xpath("//DIV[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/A[1]/DIV[1]/CITE[1]")).click();
 
         String title_name = driver.getTitle();
 
